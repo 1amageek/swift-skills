523cdcb839719d957470b9e3a0f073238ae2db47
1amageek <tmy0x3@icloud.com>
2026-03-11 16:03:11 +0900

    Update swift-yaml to 1.0.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/1amageek/swift-yaml.git", from: "0.1.0"),
+        .package(url: "https://github.com/1amageek/swift-yaml.git", from: "1.0.0"),
     ],
     targets: [
         .target(
